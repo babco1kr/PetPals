7ffee1732e305f6a5cbcb4b49536f5a651ed065f
Vanessa <47366649+vguercio10@users.noreply.github.com>
2019-04-12 14:05:42 -0400

    logic added for select pet

--- models/pet.js
+++ models/pet.js
@@ -1,3 +1,16 @@
+$(document).ready(function() {
+  var pet = $("#animal")
+    .find(":selected")
+    .val();
+  $.ajax("/search/" + pet, {
+    type: "GET"
+  }).then(function() {
+    console.log("petviews");
+    // Reload the page to get the updated list
+    location.reload();
+  });
+});
+
 // user-account on click funcition
 $('#user-account').click(function(){
     //do something

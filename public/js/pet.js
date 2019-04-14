53bac243c499625feeb40fb56d9acf9d662d35bd
Kyle Babcock <kylerbabcock@gmail.com>
2019-04-14 15:50:39 -0400

    finished notifications for requests

--- public/js/pet.js
+++ public/js/pet.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+  //Needed for select drop down to work
   $("select").formSelect();
 
+  //Call to check if currenct user has any pending requests and updates the page
+  $.get("/api/requests").then(function(data) {
+    var requestNumber = data.length;
+    $("#requests").text("(" + requestNumber + ") Requests");
+  });
+
+  // Allows filter for pet type and location
   $("#selectpetbtn").on("click", function(event) {
     event.stopImmediatePropagation();
     var pet = $("#animal")
@@ -13,6 +21,7 @@ $(document).ready(function() {
     window.location.href = "/search/" + pet + "/" + location;
   });
 
+  // Calls the route for the current users page
   $("#users-account").on("click", function(event) {
     event.preventDefault();
     var id = $(this).data("id");
@@ -20,6 +29,14 @@ $(document).ready(function() {
     window.location.href = "/user/" + id;
   });
 
+  $("#requests").on("click", function(event) {
+    event.preventDefault();
+    var id = $("#users-account").data("id");
+
+    window.location.href = "/requests/" + id;
+  });
+
+  // Adds pet to the pets table
   $("#petInsert").on("submit", function(event) {
     event.stopImmediatePropagation();
     //Validate here
@@ -52,6 +69,7 @@ $(document).ready(function() {
     });
   });
 
+  // Call for selecting pet to sit
   $("#selectPet").on("click", function(event) {
     event.stopImmediatePropagation();
     var id = $(this).data("id");
